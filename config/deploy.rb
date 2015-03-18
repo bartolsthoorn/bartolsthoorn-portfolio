e2eb845caa8b8bf01ad84baeac24bfad14e70b0d
Bart Olsthoorn <bart@Barts-MacBook-Pro.local>
2015-03-18 11:39:04 +0100

    Update portfolio to profile page

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,18 +1,40 @@
-set :user, 'bart'
-set :application, 'bartolsthoorn'
+# config valid only for current version of Capistrano
+lock '3.4.0'
 
-# Deploy in ~ and via cache
 set :use_sudo, false
-default_run_options[:pty] = true
-ssh_options[:forward_agent] = true
+set :pty, true
 set :deploy_via, :remote_cache
-
-set :repository, 'git@github.com:bartolsthoorn/bartolsthoorn-portfolio.git'
-
-set :default_environment, {  
-  'LANG' => 'en_US.UTF-8'  
-}
-
-server '109.74.192.211', :app, :web, :db, :primary => true
-
+set :ssh_options, {forward_agent: true}
+set :application, 'bartolsthoorn'
+set :repo_url, 'git@github.com:bartolsthoorn/bartolsthoorn-portfolio.git'
 set :deploy_to, '/home/bart/apps/bartolsthoorn'
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+# set :deploy_to, '/var/www/my_app_name'
+
+# Default value for :scm is :git
+# set :scm, :git
+
+# Default value for :format is :pretty
+# set :format, :pretty
+
+# Default value for :log_level is :debug
+# set :log_level, :debug
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+
+# Default value for linked_dirs is []
+# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
